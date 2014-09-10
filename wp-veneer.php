502283104952cf59fec622d621f67cbffa7a9f60
andypotanin <andy.potanin@usabilitydynamics.com>
2014-09-10 03:45:45 -0400

    0.6.4

--- wp-veneer.php
+++ wp-veneer.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP-Veneer
  * Plugin URI: http://usabilitydynamics.com/
  * Description: Veneer.io Helper.
- * Version: 0.6.2
+ * Version: 0.6.4
  * Author: Usability Dynamics
  * Author URI: http://usabilitydynamics.com/
  * License: GPLv2 or later
