89fcc16c921abc53a9166a5516e2436696b56320
andypotanin <andy@udx.io>
2014-11-23 20:58:22 -0500

    improved css enqueue

--- lib/class-bootstrap.php
+++ lib/class-bootstrap.php
@@ -570,7 +570,7 @@ namespace UsabilityDynamics\Veneer {
 			public function enqueue_scripts() {
 
 				if( is_user_logged_in() ) {
-					wp_enqueue_style( 'wp-veneer', plugins_url( '/static/styles/wp-veneer.css', dirname( __FILE__ ) ) );
+					wp_enqueue_style( 'wp-veneer', str_replace( array( 'http://', 'https://' ), '//', plugins_url( '/static/styles/wp-veneer.css', dirname( __FILE__ ) ) ) );
 				};
 
 			}
